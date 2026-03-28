4622af2a1be0038edec5a99d8bed753dce9b8e5b
Shankar <168357309+Shankarrc@users.noreply.github.com>
2026-03-29 00:46:32 +0530

    Add allowed methods to CORS configuration

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
+    methods: ["GET","POST","PUT","DELETE","OPTIONS"],
     origin: "https://chat-box-client-one.vercel.app",
     credentials: true,
   })
